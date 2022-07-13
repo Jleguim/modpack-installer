8b582517a9c724dfd6f15ba83719ec42ef21ded4
Javier Leguizamo <jlegui745@gmail.com>
2022-07-13 00:47:30 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,7 @@
 var fs = require('fs')
+const path = require('path')
+
+const imagePath = path.join(__dirname, '/public/img/image.png')
 
 module.exports = {
     modpack_name: '1.8.9 PvP Mods',
@@ -8,7 +11,7 @@ module.exports = {
     branch: 'mods',
     name: 'modpack-installer',
 
-    image: 'data:image/png;base64,' + fs.readFileSync('./public/img/image.png', 'base64'),
+    image: 'data:image/png;base64,' + fs.readFileSync(imagePath, 'base64'),
     forgeVersion: '1.8.9-forge1.8.9-11.15.1.2318-1.8.9',
     javaArgs: '-Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M'
 }
