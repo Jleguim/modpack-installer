4ac7b8036c4540e845c9c51b66cf525859374d7b
Javier Leguizamo <jlegui745@gmail.com>
2022-07-13 00:40:08 -0500

    Example modpack

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 var fs = require('fs')
 
 module.exports = {
-    modpack_name: 'My ModPack',
-    profile_dir: 'myModpack',
+    modpack_name: '1.8.9 PvP Mods',
+    profile_dir: 'pvp-mods',
 
-    repo: 'https://github.com/Jleguim/chimba-installer/',
+    repo: 'https://github.com/Jleguim/modpack-installer/',
     branch: 'mods',
-    name: 'chimba-installer',
+    name: 'modpack-installer',
 
     image: 'data:image/png;base64,' + fs.readFileSync('./public/img/image.png', 'base64'),
-    forgeVersion: '1.16.5-forge-36.2.34',
+    forgeVersion: '1.8.9-forge1.8.9-11.15.1.2318-1.8.9',
     javaArgs: '-Xmx4G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M'
 }
